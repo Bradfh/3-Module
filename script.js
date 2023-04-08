46a8d8cb7dc51b8575b6071f08c7aa9ae1ada78e
Bradfh <128874401+Bradfh@users.noreply.github.com>
2023-04-08 15:27:36 -0500

    Final product formatting plus README

--- script.js
+++ script.js
@@ -2,15 +2,15 @@ let generateBtn = document.querySelector("#generate");
 generateBtn.addEventListener("click", writePassword);
 
 function generatePassword() {
-let textUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('');
-let textLower = "abcdefghijklmnopqrstuvwxyz".split('');
-let textNum = "1234567890".split('');
-let textChar = '!@#$%^&*?'.split('');
-let newPassword = [];
-let megaArray = [];
-let x = [];
-let passwordLength = "";
-passwordLength = prompt("How long would you like your password to be?  Please enter a number between 8 and 128.");
+  let textUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('');
+  let textLower = "abcdefghijklmnopqrstuvwxyz".split('');
+  let textNum = "1234567890".split('');
+  let textChar = '!@#$%^&*?'.split('');
+  let newPassword = [];
+  let megaArray = [];
+  let x = [];
+  let passwordLength = "";
+  passwordLength = prompt("How long would you like your password to be?  Please enter a number between 8 and 128.");
   if (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
     alert("Please enter a valid length for your password.  Choose a number between 8 and 128");
     generatePassword();
@@ -41,15 +41,15 @@ passwordLength = prompt("How long would you like your password to be?  Please en
     }
     for (let i = x.length; i < passwordLength; i++) {
       newPassword += (megaArray[Math.floor(Math.random(i) * (megaArray.length))]);
-      }  
-    } 
-    
-    for (let i = 0; i < x.length; i++) {
-      newPassword += x[i];
     }
-    
-    return newPassword
   }
+
+  for (let i = 0; i < x.length; i++) {
+    newPassword += x[i];
+  }
+
+  return newPassword
+}
 
 
 function writePassword() {
